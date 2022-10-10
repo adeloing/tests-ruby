cf612124a38c80349dc852435bac67ca4f355759
adeloing <adeloing@gmail.com>
2022-10-10 15:20:51 +0200

    ok

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,3 +1,3 @@
 def time_string(i)
-    i / 60 < 1 ? "00:00:" + format('%02d',i) : i < 3600 ? "00:" + format('%02d',i/60) + ":" + format('%02d',i%60) : format('%02d',i/3600) + ":" + format('%02d',(i-i/3600*3600)/60) + ":" + format('%02d',i%60)
+    Time.at(i).utc.strftime("%H:%M:%S")
 end
